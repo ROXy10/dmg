c80c5b5e8335e07ee02230d63c4e923f7da0f0ad
ROXy <roxy@mail.ru>
2016-03-26 16:39:21 +0200

    first commit

--- dmg/settings.py
+++ dmg/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
+from dmg.local_settings import *
 
 import os
 
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'dmg', 'media')
+
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'dmg', 'static')
